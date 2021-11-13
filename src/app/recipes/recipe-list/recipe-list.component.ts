c7871750206b27ad4ba48dc5096d945e7004ca0c
matthewrapp <mattrapp25@gmail.com>
2021-11-13 10:34:15 -0700

    done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -17,7 +17,7 @@ export class RecipeListComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-    this.subscription = this.recipeService.recipesChange.subscribe((recipes: Recipe[]) => {
+    this.subscription = this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     })
   }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,26 +7,31 @@ import { Recipe } from "./recipe.model";
 
 @Injectable({ providedIn: 'root' })
 export class RecipeService {
-    recipesChange = new Subject<Recipe[]>();
+    recipesChanged = new Subject<Recipe[]>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe',
-            'This is simply a test.',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8yMIHkxs901EzRfpvMq6gIEfDADZcfbJxLw&usqp=CAU',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe('A Test Recipe',
-            'This is simply a test.',
-            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBlaxE2hcmTVZ6kb0quZ1gX8YuUuekl31caA&usqp=CAU',
-            [
-                new Ingredient('Chicken', 2),
-                new Ingredient('Potato Chips', 4)
-            ]),
+        // new Recipe('A Test Recipe',
+        //     'This is simply a test.',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8yMIHkxs901EzRfpvMq6gIEfDADZcfbJxLw&usqp=CAU',
+        //     [
+        //         new Ingredient('Meat', 1),
+        //         new Ingredient('French Fries', 20)
+        //     ]),
+        // new Recipe('A Test Recipe',
+        //     'This is simply a test.',
+        //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBlaxE2hcmTVZ6kb0quZ1gX8YuUuekl31caA&usqp=CAU',
+        //     [
+        //         new Ingredient('Chicken', 2),
+        //         new Ingredient('Potato Chips', 4)
+        //     ]),
     ];
 
     constructor(private shoppingListService: ShoppingListService) { }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice())
+    }
 
     getRecipes() {
         return this.recipes.slice();
@@ -42,16 +47,16 @@ export class RecipeService {
 
     addRecipe(recipe: Recipe) {
         this.recipes.push(recipe);
-        this.recipesChange.next(this.recipes.slice());
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     updateRecipe(index: number, newRecipe: Recipe) {
         this.recipes[index] = newRecipe;
-        this.recipesChange.next(this.recipes.slice());
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     deleteRecipe(index: number) {
         this.recipes.splice(index, 1);
-        this.recipesChange.next(this.recipes.slice());
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
